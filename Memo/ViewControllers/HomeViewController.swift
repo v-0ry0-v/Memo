d19a930c929595cd319efaf70e58f01c9f778d47
v-0ry0-v <v.0ry0.v@gmail.com>
2022-10-07 06:45:23 +0900

    setup Memo project

---
+++ Memo/ViewControllers/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  Memo
+//
+//  Created by Ryoya Ogura on 2022/10/07.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViews()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .white
+    }
+}
