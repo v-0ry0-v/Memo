99a46e8d5660b07da82d2c39f5dd9c4cd7cc2c49
v-0ry0-v <v.0ry0.v@gmail.com>
2022-10-07 06:43:00 +0900

    create Memo project

---
+++ Memo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by Ryoya Ogura on 2022/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
